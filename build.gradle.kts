8d6b59fb30f4de14f1682a6f24364929780c2a56
Simone Stefani <s.stefani95@gmail.com>
2020-05-23 15:56:02 +0200

    Setup Ktor project

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.70"
+}
+
+group = "dev.simonestefani"
+version = "0.0.1"
+
+application {
+    mainClassName = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenLocal()
+    jcenter()
+    maven { url = uri("https://kotlin.bintray.com/ktor") }
+}
+
+dependencies {
+    // BOMs
+    implementation(platform("io.ktor:ktor-bom:$ktor_version"))
+
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+
+    // Ktor
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-locations")
+    implementation("io.ktor:ktor-server-sessions")
+    implementation("io.ktor:ktor-auth")
+    implementation("io.ktor:ktor-auth-jwt")
+    implementation("io.ktor:ktor-gson")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // Testing
+    testImplementation("io.ktor:ktor-server-tests")
+}
+
+kotlin.sourceSets["main"].kotlin.srcDirs("src")
+kotlin.sourceSets["test"].kotlin.srcDirs("test")
+
+sourceSets["main"].resources.srcDirs("resources")
+sourceSets["test"].resources.srcDirs("testresources")
